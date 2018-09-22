25663c2be3c8abb85a12a5effb2c27d98c2a4453
cipriancosarca19 <cipri.cosarca@gmail.com>
2018-09-22 17:09:54 +0300

    Update Spinner.js

--- src/components/UI/Spinner/Spinner.js
+++ src/components/UI/Spinner/Spinner.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import classes from './Spinner.css';
-const spinner = () => <div className={classes.Loader}>Loading...</div>;
+const spinner = () => <div className={classes.Loader}>Se incarca...</div>;
 
 export default spinner;
